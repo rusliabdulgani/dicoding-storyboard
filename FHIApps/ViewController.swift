1548d1ea9636da6bae06f0c7f25354fef2adc9c1
abdulgani88 <abdul.gani@fullertonhealth.com>
2022-03-22 07:53:00 +0700

    Initial Commit

---
+++ FHIApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FHIApps
+//
+//  Created by FHI-IT-Dev on 22/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
