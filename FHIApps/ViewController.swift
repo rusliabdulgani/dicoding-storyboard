c34451385d53ff0d29c4ece85a5fa9ef9763458e
abdulgani88 <abdul.gani@fullertonhealth.com>
2022-04-01 07:13:06 +0700

    first time commit

--- FHIApps/ViewController.swift
+++ FHIApps/ViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func EditProfile(_ sender: Any) {
+        let alert = UIAlertController(
+                title: "Apakah Anda berniat untuk mengubah profil?",
+                message: "Anda dapat memperbarui profil Anda sekarang juga.",
+                preferredStyle: .alert
+            )
+        alert.addAction(UIAlertAction(title: "Ya", style: .default) { _ in
+                print("Mengganti profile.")
+            })
+        
+        alert.addAction(UIAlertAction(title: "Tidak", style: .cancel) { _ in
+                print("Membatalkan ubah profil.")
+            })
+         
+            self.present(alert, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
